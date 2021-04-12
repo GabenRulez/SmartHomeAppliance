49c9357d4b6ede3f7576d0e12f449e5b64dc4dfe
GabenRulez <45923220+GabenRulez@users.noreply.github.com>
2021-04-12 22:05:44 +0200

    Cleanup++

--- code/SmartHomeAppliance/SmartHomeAppliance.ino
+++ code/SmartHomeAppliance/SmartHomeAppliance.ino
@@ -9,102 +9,45 @@ This is a main file of our project for Embedded Systems course in AGH Kraków.
 #include <Adafruit_SSD1306.h>
 
 /*  OLED SCREEN   */
-#define Display_SCL_Pin 22       // IO22
-#define Display_SDA_Pin 21       // IO21
-#define SCREEN_WIDTH 128          // OLED display width, in pixels
-#define SCREEN_HEIGHT 32          // OLED display height, in pixels
-#define OLED_RESET     4          // Reset pin # (or -1 if sharing Arduino reset pin)
-#define SCREEN_ADDRESS 0x3C       ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
+#define Display_SCL_Pin   22       // IO22
+#define Display_SDA_Pin   21       // IO21
+#define SCREEN_WIDTH      128      // OLED display width, in pixels
+#define SCREEN_HEIGHT     32       // OLED display height, in pixels
+#define OLED_RESET        4        // Reset pin # (or -1 if sharing Arduino reset pin)
+#define SCREEN_ADDRESS    0x3C     //< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 /*  ROTARY ENCODER  */
-#define BUTTON_PIN 19
-#define LEFT_PIN 18
-#define RIGHT_PIN 5
+#define ROTARY_ENCODER_BUTTON_PIN   19
+#define ROTARY_ENCODER_PIN_A        18
+#define ROTARY_ENCODER_PIN_B        5
+TaskHandle_t rotaryEncoderInterruptTaskHandle = NULL;
+const int rotaryEncoderDebounceTime = 33;
+int rotaryEncoderValue = 0;
 
 
 void initializeScreen();
 void initializeDisplay();
 void initializeRotaryEncoder();
+void initializeRotaryEncoderInterrupt();
+void rotaryEncoderInterrupt();
+void rotaryEncoderInterruptInISR(void *pvParameter1, uint32_t ulParameter2);
+void rotaryEncoderDebouncedHandler(void *pinBState);
+void initializeMutex();
 
-SemaphoreHandle_t mutex_v;
 
-  
 void setup() {
   Serial.begin(115200);
   initializeScreen();
   initializeDisplay();
   initializeRotaryEncoder();
-
-  /*display.cp437(true);*/
-
-  //attachInterrupt(LEFT_PIN, interruptFunction, RISING);
-  mutex_v = xSemaphoreCreateMutex();
-  attachInterrupt(LEFT_PIN, ehe, RISING);
-  //attachInterrupt(RIGHT_PIN, b_interrupt, CHANGE);
-}
-
-int result = 0;
-int lastMillis = 0;
-
-
-TaskHandle_t taskHandle = NULL;
-int debounceTime = 30;
-
-void ehe(){
-  int pin_right = digitalRead(RIGHT_PIN);
-  //Serial.println(pin_right);
-  BaseType_t xHigherPriorityTaskWoken = pdFALSE;
-  BaseType_t xInterfaceToService = NULL;
-
-  xTimerPendFunctionCallFromISR(
-    debounceInterrupt,
-    (void*)pin_right,
-    //NULL,
-    ( uint32_t) xInterfaceToService,
-    &xHigherPriorityTaskWoken  
-  );
-  portYIELD_FROM_ISR();
-}
-
-void debounceInterrupt(void *pvParameter1, uint32_t ulParameter2) {
-  BaseType_t xInterfaceToService = (BaseType_t) ulParameter2;
-  
-  if(taskHandle != NULL){
-    vTaskDelete(taskHandle);
-  }
-  int right_pin_value = (int) pvParameter1;
-  xTaskCreatePinnedToCore(
-    betterInterrupt,
-    "debouncedTask",
-    1000,
-    (void*) right_pin_value,
-    1,
-    &taskHandle,
-    0
-  );
-}
-
-void betterInterrupt(void *gay){
-  vTaskDelay(debounceTime);
-  Serial.println((int)gay);
-  if((int)gay == LOW) result++;
-  else result--;
-  taskHandle = NULL;
-  vTaskDelete(NULL);
+  initializeRotaryEncoderInterrupt();
 }
 
 void loop() {
-  //for(;;);
-  //Serial.println("Button:" + String(digitalRead(BUTTON_PIN)) + "\tLeft:" + String(digitalRead(LEFT_PIN)) + "\tRight:" + String(digitalRead(RIGHT_PIN)) + "\tCzas:" + String(millis()));
   display.clearDisplay();
   display.setCursor(0,0);
-
-  //xSemaphoreTake(mutex_v, 1000);
-  int myResult = result;
-  //xSemaphoreGive(mutex_v);
-  //if(random(10) < 3) debounceInterrupt();
-  display.println(myResult);
+  display.println(rotaryEncoderValue);
   display.display();
   delay(10);
 }
@@ -121,12 +64,53 @@ void initializeDisplay(){
   display.setTextSize(1);
   display.clearDisplay();
   display.setCursor(0,0);
-  display.println("Projekt z Systemow Wbudowanych");
+  display.println("# Embedded  Systems #\n# ---- Project ---- #");
   display.display();
+  delay(2000);
+  /*display.cp437(true);*/
 }
 
 void initializeRotaryEncoder(){
-  pinMode(BUTTON_PIN, INPUT_PULLDOWN);
-  pinMode(LEFT_PIN, INPUT_PULLDOWN);
-  pinMode(RIGHT_PIN, INPUT_PULLDOWN);
+  pinMode(ROTARY_ENCODER_BUTTON_PIN, INPUT_PULLDOWN);
+  pinMode(ROTARY_ENCODER_PIN_A,   INPUT_PULLDOWN);
+  pinMode(ROTARY_ENCODER_PIN_B,  INPUT_PULLDOWN);
+}
+
+void initializeRotaryEncoderInterrupt(){
+  attachInterrupt(ROTARY_ENCODER_PIN_A, rotaryEncoderInterrupt, RISING);
+}
+
+void rotaryEncoderInterrupt(){
+  int pinBState = digitalRead(ROTARY_ENCODER_PIN_B);
+  BaseType_t xHigherPriorityTaskWoken = pdFALSE;
+  BaseType_t xInterfaceToService = NULL;
+  
+  xTimerPendFunctionCallFromISR( rotaryEncoderInterruptInISR, (void*)pinBState, ( uint32_t) xInterfaceToService, &xHigherPriorityTaskWoken );
+  portYIELD_FROM_ISR();
+}
+
+void rotaryEncoderInterruptInISR(void *pvParameter1, uint32_t ulParameter2) {
+  BaseType_t xInterfaceToService = (BaseType_t) ulParameter2;
+  
+  if(rotaryEncoderInterruptTaskHandle != NULL){ vTaskDelete(rotaryEncoderInterruptTaskHandle); }
+  
+  int pinBState = (int) pvParameter1;
+  xTaskCreatePinnedToCore( rotaryEncoderDebouncedHandler, "debouncedTask", 1000, (void*) pinBState, 1, &rotaryEncoderInterruptTaskHandle, 0 );
+}
+
+void rotaryEncoderDebouncedHandler(void *pinBState){
+  vTaskDelay(rotaryEncoderDebounceTime);
+  
+  if((int)pinBState == LOW) rotaryEncoderValue++;
+  else rotaryEncoderValue--;
+  
+  rotaryEncoderInterruptTaskHandle = NULL;
+  vTaskDelete(NULL);
+}
+
+void initializeMutex(){
+  //SemaphoreHandle_t mutex_v;
+  //mutex_v = xSemaphoreCreateMutex();
+  //xSemaphoreTake(mutex_v, 1000);
+  //xSemaphoreGive(mutex_v);
 }
