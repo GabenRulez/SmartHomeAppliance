1c5320782da608a4a53ee5b0adfb311c86d122e3
GabenRulez <45923220+GabenRulez@users.noreply.github.com>
2021-05-03 18:01:29 +0200

    Added template secretConfig.h

--- code/SmartHomeAppliance/secretConfig.h
+++ code/SmartHomeAppliance/secretConfig.h
@@ -1,6 +1,10 @@
 #ifndef secretConfigFile
 #define secretConfigFile
 
+#define ST_SSID       "ssid"  // Station SSID (so the name of the network, that you're gonna connect to)
+#define ST_PASSWORD   "password"
 
+#define AP_SSID       "SmartHomeAppliance"  // Access Point SSID (so the name of the network, that other devices could connect to)
+#define AP_PASSWORD   "password"
 
 #endif
